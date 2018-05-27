ebb9e537734a091b88554a4b1a7f0894998dcc14
Charvak <ckondapalli@wpi.edu>
2018-05-26 21:22:05 -0400

    Added score on screen

--- snake.py
+++ snake.py
@@ -1,10 +1,13 @@
 import pygame
+import math
 
-speed = 2
-growth = 10
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-ScreenSize = width, height = 640, 480
+colors = {
+            'BLACK': (0, 0, 0),
+            'WHITE': (255, 255, 255)
+          }
+screen_size = width, height = 640, 480
+score_pos = (width * 7/10, 15)
+score_per_food = 10
 # Direction of snake: 0 = right, 1 = up, 2 = left, 3 = down
 
 class Snake:
@@ -13,6 +16,8 @@ class Snake:
         self.direction = 0
         self.length = 30
         self.width = 5
+        self.growth = 10
+        self.speed = 2
 
     def turn_left(self):
         self.direction += 1
@@ -38,33 +43,30 @@ class Snake:
         if self.direction == 3:
             tail = [head[0], head[1] - self.length]
 
-       
-            
-        pygame.draw.line(screen, WHITE, head, tail, self.width)
+        pygame.draw.line(screen, colors['WHITE'], head, tail, self.width)
 
     def move_forward(self):
         if self.direction == 0:
-            self.position[0] += speed
+            self.position[0] += self.speed
 
         if self.direction == 1:
-            self.position[1] -= speed
+            self.position[1] -= self.speed
 
         if self.direction == 2:
-            self.position[0] -= speed
+            self.position[0] -= self.speed
 
         if self.direction == 3:
-            self.position[1] += speed
+            self.position[1] += self.speed
 
+        # Check for screen bounds
         if self.position[0] > width:
             self.position[0] = 0
 
         if self.position[0] < 0:
             self.position[0] = width
-            
 
         if self.position[1] > height:
             self.position[1] = 0
-           
 
         if self.position[1] < 0:
             self.position[1] = height
@@ -79,18 +81,27 @@ class FoodPellet:
 
     def draw(self, screen):
         if self.alive:
-            pygame.draw.circle(screen, WHITE, self.position, self.radius, 0)
+            pygame.draw.circle(screen, colors['WHITE'], self.position, self.radius, 0)
 
+def distance(a, b):
+    return math.sqrt( (a[0]-b[0])**2 + (a[1]-b[1])**2 )
+
+def is_eating(snake, food):
+    ''' Checks if the snake is in eating range  of the food '''
+    return distance(snake.position, food.position) <= food.radius
+
+def draw_score(screen, score):
+    font = pygame.font.SysFont('UbuntuMono-R', 30)
+    text_surface = font.render('score:  ' + str(score), False, colors['WHITE'])
+    screen.blit(text_surface, score_pos)
 
 def main():
     pygame.init()
 
-
-    
-    screen = pygame.display.set_mode(ScreenSize)
+    screen = pygame.display.set_mode(screen_size)
 
     snake = Snake()
-
+    score = 0
     run = True
     clock = pygame.time.Clock()
 
@@ -110,12 +121,19 @@ def main():
                 if event.key == pygame.K_RIGHT:
                     snake.turn_right()
 
-        screen.fill(BLACK)
+        screen.fill(colors['BLACK'])
 
-        food.draw(screen)
         snake.move_forward()
-        snake.draw(screen)
 
+        if is_eating(snake, food):
+            score += score_per_food
+
+        def draw_everything():
+            food.draw(screen)
+            snake.draw(screen)
+            draw_score(screen, score)
+
+        draw_everything()
         pygame.display.flip()
 
 if __name__ == '__main__':
